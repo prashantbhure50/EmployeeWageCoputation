13989ff3c5b04a3c8e26e23037aa350fb1e93cc7
Prashant Bhure <Prashantbhure50@gmail.com>
2021-04-19 02:02:36 +0530

    Employe file is Added

--- EmployeeWageCoputation/Program.cs
+++ EmployeeWageCoputation/Program.cs
@@ -6,15 +6,32 @@ namespace EmployeeWageCoputation
     {
         static void Main(string[] args)
         {
-            CConsole.WriteLine("Welcome To Employee Wages Computation ");
+
+            Console.WriteLine("Welcome To Employee Wages Computation ");
 
             int WagePerHour = 20;
             int FullDay = 8;
-            int Month = 20;
-            int totalOneMonth = Month * FullDay * WagePerHour;
+            Random random = new Random();
+            int Hours = 0;
+            int days = 0;
+            int totalOneMonth = 0;
+            while (days < 20 && Hours < 100)
+            {
+                int Check = random.Next(0, 2);
+                if (Check == 0)
+                {
+                    days++;
+                }
+                else
+                {
+                    Hours++;
+                }
+                totalOneMonth = totalOneMonth + FullDay * WagePerHour;
+            }
             Console.WriteLine(totalOneMonth);
-            Console.ReadKey();
 
+
+            Console.ReadKey();
         }
     }
 }
